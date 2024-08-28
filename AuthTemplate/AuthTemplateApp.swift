9034eb28293bc6ce2a7d628ceb28dff10f214481
Pei <peitzuh44@berkeley.edu>
2024-08-28 12:56:05 +0800

    Initial Commit

---
+++ AuthTemplate/AuthTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTemplateApp.swift
+//  AuthTemplate
+//
+//  Created by Pei-Tzu Huang on 2024/8/28.
+//
+
+import SwiftUI
+
+@main
+struct AuthTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthTemplate
+//
+//  Created by Pei-Tzu Huang on 2024/8/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
